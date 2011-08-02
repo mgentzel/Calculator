9c05f9738119737927929caa4752a211809c58c3
Mark Gentzel <markgentzel@gmail.com>
2011-08-02 13:33:30 -0400

    Initial Commit

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Mark Gentzel on 8/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController {
+    
+}
+
+@end
---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,17 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by Mark Gentzel on 8/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface CalculatorTests : SenTestCase {
+@private
+    
+}
+
+@end
